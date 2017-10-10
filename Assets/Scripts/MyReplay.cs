b018222d64dab338d23cc70dd03a21851c98aafb
Jeremy 'Dieedi' Pouillot <pouillot.j@free.fr>
2017-10-10 06:56:32 +0200

    save

---
+++ Assets/Scripts/MyReplay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyReplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class MyKeyFrame {
+
+    private float time;
+    private Vector3 pos;
+    private Quaternion rot;
+
+    public MyKeyFrame (float frameTime, Vector3 framePos, Quaternion frameRot) {
+        time = frameTime;
+        pos = framePos;
+        rot = frameRot;
+
+        Debug.Log ("time : " + time + ", pos : " + pos + ", rot : " + rot);
+
+    }
+}
---
+++ Assets/SplitTwo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitTwo : MonoBehaviour {
+
+    [Tooltip("Offset between main ball and splited ball")]
+    public float offset;
+
+    [Tooltip("Main ball")]
+    public GameObject MainSphere;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 spherePosition = MainSphere.GetComponent<Transform> ().position;
+        Vector3 newPosition = new Vector3 (spherePosition.x, spherePosition.y + offset, spherePosition.z);
+        transform.position = newPosition;
+        transform.rotation = MainSphere.GetComponent<Transform> ().rotation;
+	}
+}
